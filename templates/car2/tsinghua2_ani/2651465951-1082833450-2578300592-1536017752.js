80354cae8441926bc48bc57b3fd7a2a4ebf5f445
Gux12 <gx199492@qq.com>
2016-04-15 14:28:31 +0800

    修改了car1,car2,cloth1,cloth2的尺寸以及内容，更新了js文件，添加了动画相机

---
+++ templates/car2/tsinghua2_ani/2651465951-1082833450-2578300592-1536017752.js
@@ -0,0 +1,1 @@
+{"metadata":{"formatVersion":3.10,"generatedBy":"EmpBuilder Beta 16.0327.207","name":"Camera003","sourceFile":"Camera003"}}
---
+++ templates/cloth2/tsinghua2_ani/371831383-1168137870-3158520982-2145131910.js
@@ -0,0 +1,1 @@
+{"metadata":{"formatVersion":3.10,"generatedBy":"EmpBuilder Beta 16.0327.207","name":"Camera03","sourceFile":"Camera03"}}
